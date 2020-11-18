60914785e6b37355f1d5de1cdf00bdc5d4aa2695
fbyter <fbyter@qq.com>
2020-11-19 02:14:55 +0800

    JZ32 py

---
+++ python/JZ32.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+class Solution:
+    def Power(self, base, exponent):
+        # write code here
+        if base==0:
+            return 0
+        elif exponent==0:
+            return 1
+        else:
+            ret=1
+            for i in range(abs(exponent)):
+                ret *= base
+
+            print(ret)
+
+            return ret if exponent>0 else 1.0/ret
